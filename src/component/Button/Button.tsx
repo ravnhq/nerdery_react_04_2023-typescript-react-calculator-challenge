6b00bb1dbb81c7a631003c94d9530f8568863cbd
Miguel Rodriguez <fm.rodriguezg@hotmail.com>
2023-04-19 21:43:38 -0600

    fix: removed unused code and change props

--- src/component/Button/Button.tsx
+++ src/component/Button/Button.tsx
@@ -1,37 +1,11 @@
 import React from 'react'
-
-// export default class Button extends React.Component {
-//   static propTypes = {
-//     name: PropTypes.string,
-//     orange: PropTypes.bool,
-//     wide: PropTypes.bool,
-//     clickHandler: PropTypes.func,
-//   }
-
-//   handleClick = () => {
-//     this.props.clickHandler(this.props.name)
-//   }
-
-//   render() {
-//     const className = [
-//       'component-button',
-//       this.props.orange ? 'orange' : '',
-//       this.props.wide ? 'wide' : '',
-//     ]
-
-//     return (
-//       <div className={className.join(' ').trim()}>
-//         <button onClick={this.handleClick}>{this.props.name}</button>
-//       </div>
-//     )
-//   }
-// }
+import './index.css'
 
 interface Props {
   name: string
-  orange: boolean
-  wide: boolean
-  clickHandler: (str: string) => void
+  orange?: boolean
+  wide?: boolean
+  clickHandler: () => void
 }
 
 const Button: React.FunctionComponent<Props> = ({
