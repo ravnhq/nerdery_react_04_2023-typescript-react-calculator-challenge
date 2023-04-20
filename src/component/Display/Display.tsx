66e49bd6c79b8ac8f0fdf7cd700d590d2e47ef81
Miguel Rodriguez <fm.rodriguezg@hotmail.com>
2023-04-19 21:45:05 -0600

    feat: convert Display component to TS

---
+++ src/component/Display/Display.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './index.css'
+
+interface Props {
+    value: string
+}
+
+const Display: React.FunctionComponent<Props> = ({ value }) => (
+  <div className="component-display">
+    <div>{value}</div>
+  </div>
+)
+
+export default Display
