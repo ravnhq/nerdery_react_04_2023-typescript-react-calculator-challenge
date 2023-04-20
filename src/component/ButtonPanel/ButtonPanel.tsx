cd0ef7fd7fd611e5cf866911ab1905372176f119
Miguel Rodriguez <fm.rodriguezg@hotmail.com>
2023-04-19 21:44:30 -0600

    fix: passing props to button component correctly

--- src/component/ButtonPanel/ButtonPanel.tsx
+++ src/component/ButtonPanel/ButtonPanel.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Button } from '../Button'
+import './index.css'
 
 interface Props {
   clickHandler: (str: string) => void
@@ -12,33 +13,33 @@ const ButtonPanel: React.FunctionComponent<Props> = ({ clickHandler }) => {
   return (
     <div className="component-button-panel">
       <div>
-        <Button name="AC" clickHandler={handleClick} />
-        <Button name="+/-" clickHandler={handleClick} />
-        <Button name="%" clickHandler={handleClick} />
-        <Button name="÷" clickHandler={handleClick} orange />
+        <Button name="AC" clickHandler={() => handleClick('AC')} />
+        <Button name="+/-" clickHandler={() => handleClick('+/-')} />
+        <Button name="%" clickHandler={() => handleClick('%')} />
+        <Button name="÷" clickHandler={() => handleClick('÷')} orange />
       </div>
       <div>
-        <Button name="7" clickHandler={handleClick} />
-        <Button name="8" clickHandler={handleClick} />
-        <Button name="9" clickHandler={handleClick} />
-        <Button name="x" clickHandler={handleClick} orange />
+        <Button name="7" clickHandler={() => handleClick('7')} />
+        <Button name="8" clickHandler={() => handleClick('8')} />
+        <Button name="9" clickHandler={() => handleClick('9')} />
+        <Button name="x" clickHandler={() => handleClick('x')} orange />
       </div>
       <div>
-        <Button name="4" clickHandler={handleClick} />
-        <Button name="5" clickHandler={handleClick} />
-        <Button name="6" clickHandler={handleClick} />
-        <Button name="-" clickHandler={handleClick} orange />
+        <Button name="4" clickHandler={() => handleClick('4')} />
+        <Button name="5" clickHandler={() => handleClick('5')} />
+        <Button name="6" clickHandler={() => handleClick('6')} />
+        <Button name="-" clickHandler={() => handleClick('-')} orange />
       </div>
       <div>
-        <Button name="1" clickHandler={handleClick} />
-        <Button name="2" clickHandler={handleClick} />
-        <Button name="3" clickHandler={handleClick} />
-        <Button name="+" clickHandler={handleClick} orange />
+        <Button name="1" clickHandler={() => handleClick('1')} />
+        <Button name="2" clickHandler={() => handleClick('2')} />
+        <Button name="3" clickHandler={() => handleClick('3')} />
+        <Button name="+" clickHandler={() => handleClick('+')} orange />
       </div>
       <div>
-        <Button name="0" clickHandler={handleClick} wide />
-        <Button name="." clickHandler={handleClick} />
-        <Button name="=" clickHandler={handleClick} orange />
+        <Button name="0" clickHandler={() => handleClick('0')} wide />
+        <Button name="." clickHandler={() => handleClick('.')} />
+        <Button name="=" clickHandler={() => handleClick('=')} orange />
       </div>
     </div>
   )
